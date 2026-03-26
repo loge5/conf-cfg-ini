21e254d0b901f5176277d61482ed698ab1117fe6
Rolf Loges <rolf@loges.org>
2026-03-26 07:53:29 +0100

    chore: replace mocha&chai with jest

--- conf-cfg-ini.spec.js
+++ conf-cfg-ini.spec.js
@@ -1,5 +1,3 @@
-var should = require('chai').should();
-var expect = require('chai').expect;
 var Config = require('./conf-cfg-ini');
 
 var testData = [
@@ -10,77 +8,77 @@ var testData = [
 
 describe('Config', function() {
     it('should be defined', function () {
-        should.exist(Config);
+        expect(Config).toBeDefined();
     });
-    
+
     it('setOptions should overwrite options', function () {
         var config = new Config();
         config.options.lineEnding = "\n";
         config.options.trimLines = true;
         config.setOptions({lineEnding: "\r\n", trimLines: undefined});
-        expect(config.options.lineEnding).to.equal("\r\n");
-        expect(config.options.trimLines).to.equal(true);
+        expect(config.options.lineEnding).toBe("\r\n");
+        expect(config.options.trimLines).toBe(true);
     });
-    
+
     it('detectLineEndings should detect windows style (\\r\\n)', function () {
         var config = new Config();
-        config.detectLineEnding("line1\r\nline2\r\n").should.equal("\r\n");
+        expect(config.detectLineEnding("line1\r\nline2\r\n")).toBe("\r\n");
     });
-    
+
     it('detectLineEndings should detect unix style (\\n)', function () {
         var config = new Config();
-        config.detectLineEnding("line1\nline2\n").should.equal("\n");
+        expect(config.detectLineEnding("line1\nline2\n")).toBe("\n");
     });
-    
+
     it('detectLineEndings should detect mac style (\\r)', function () {
         var config = new Config();
-        config.detectLineEnding("line1\rline2\r").should.equal("\r");
+        expect(config.detectLineEnding("line1\rline2\r")).toBe("\r");
     });
-    
+
     it('detectLineEndings should detect wtf style (\\n\\r)', function () {
         var config = new Config();
-        config.detectLineEnding("line1\n\rline2\n\r").should.equal("\n\r");
+        expect(config.detectLineEnding("line1\n\rline2\n\r")).toBe("\n\r");
     });
-    
+
     it('decode should return a object', function () {
         var config = new Config();
         for(var i = 0; i < testData.length; i++){
-            config.decode(testData[i]).should.be.a('object');
+            expect(typeof config.decode(testData[i])).toBe('object');
         }
     });
-    
+
     it('encode return should a string', function () {
         var config = new Config();
-        config.encode({'Section':{'a': 1}}).should.be.a('string');
-        config.encode({'a':1}).should.be.a('string');
-        config.encode({}).should.be.a('string');
+        expect(typeof config.encode({'Section':{'a': 1}})).toBe('string');
+        expect(typeof config.encode({'a':1})).toBe('string');
+        expect(typeof config.encode({})).toBe('string');
     });
-    
+
     it('decode should handle attributes without section', function () {
         var config = new Config();
         config.options.lineEnding = "\n";
         var result = config.decode("stray=foo\n[Section1]\na=b\n");
-        expect(result.stray).to.equal("foo");
+        expect(result.stray).toBe("foo");
     });
-    
+
     it('encode should handle attributes without section', function () {
         var config = new Config();
         config.options.lineEnding = "\n";
         var encoded = config.encode({stray:'foo','SectionA':{'a': 1}});
         var decoded = config.decode(encoded);
-        expect(decoded.stray).to.equal("foo");
+        expect(decoded.stray).toBe("foo");
     });
-    
+
     it('decode should return object with same attributes', function () {
         var data = ";comment\n[SectionA]\nkey=value\n";
         var config = new Config();
         config.options.lineEnding = config.detectLineEnding(data);
         var result = config.decode(data);
-        result.should.be.a('object');
-        should.exist(result.SectionA);
-        result.SectionA.key.should.equal("value");
+        expect(typeof result).toBe('object');
+        expect(result.SectionA).toBeDefined();
+        expect(result.SectionA.key).toBe("value");
     });
-    
+
     it('decode>encode>decode>encode return should produce consistent results', function () {
         for(var i = 0; i < testData.length; i++){
             var data = testData[i];
@@ -90,26 +88,26 @@ describe('Config', function() {
             var encoded1 = config.encode(decoded1);
             var decoded2 = config.decode(encoded1);
             var encoded2 = config.encode(decoded2);
-            expect(encoded1).to.equal(encoded2);
-            expect(decoded1).to.deep.equal(decoded2);
+            expect(encoded1).toBe(encoded2);
+            expect(decoded1).toEqual(decoded2);
         }
     });
-    
+
     it('decode should be able to handle multiple comment identifier', function () {
         var config = new Config();
         config.options.lineEnding = "\n";
         config.options.commentIdentifiers = [';','//','#'];
         var result = config.decode(";comment1\n//comment2\n#comment3\n");
-        expect(result).to.deep.equal({});
+        expect(result).toEqual({});
     });
-    
+
     it('decode should be able to handle custom assign identifier', function () {
         var config = new Config();
         config.options.lineEnding = "\n";
         config.options.assignIdentifier = ":";
         var result = config.decode("[Section]\nfoo:bar\n");
-        should.exist(result.Section);
-        expect(result.Section.foo).to.equal("bar");
+        expect(result.Section).toBeDefined();
+        expect(result.Section.foo).toBe("bar");
     });
 
     it('decode should prevent prototype pollution attacks', function () {
@@ -117,18 +115,18 @@ describe('Config', function() {
         config.options.lineEnding = "\n";
         config.options.assignIdentifier = ":";
         var result = config.decode("[__proto__]\nfoo:bar\n");
-        should.not.exist(result.__proto__.foo);
+        expect(result.__proto__.foo).toBeUndefined();
         result = config.decode("[Section]\n__proto__:bar\n");
-        expect(result.Section.__proto__).to.not.equal("bar");
+        expect(result.Section.__proto__).not.toBe("bar");
     });
 
     it('valueTrim should trim custom chars', function () {
         var config = new Config();
-        expect(config.valueTrim('"Te"s"t"', '"')).to.equal('Te"s"t');
-        expect(config.valueTrim('"Te"s"t"', '')).to.equal('"Te"s"t"');
-        expect(config.valueTrim('"Te"s"t"', '#')).to.equal('"Te"s"t"');
-        expect(config.valueTrim('""Te"s"t""', '""')).to.equal('"Te"s"t"');
-        expect(config.valueTrim('[Te"s"t]', '[]')).to.equal('Te"s"t');
+        expect(config.valueTrim('"Te"s"t"', '"')).toBe('Te"s"t');
+        expect(config.valueTrim('"Te"s"t"', '')).toBe('"Te"s"t"');
+        expect(config.valueTrim('"Te"s"t"', '#')).toBe('"Te"s"t"');
+        expect(config.valueTrim('""Te"s"t""', '""')).toBe('"Te"s"t"');
+        expect(config.valueTrim('[Te"s"t]', '[]')).toBe('Te"s"t');
     })
 
     it('valueIdentifiers should trimed or added', function () {
@@ -137,10 +135,10 @@ describe('Config', function() {
         config.options.lineEnding = "\n";
         config.options.valueIdentifier = "'"
         var result = config.decode(data);
-        expect(result.SectionA.key1).to.equal("val1");
-        expect(result.SectionA.key2).to.equal("val2");
+        expect(result.SectionA.key1).toBe("val1");
+        expect(result.SectionA.key2).toBe("val2");
         var data2 = config.encode(result);
-        expect(data2).to.equal(data);
+        expect(data2).toBe(data);
     })
 
     it('ignoreMultipleAssignIdentifier should ignore multiple assing identifiers', function () {
@@ -151,8 +149,8 @@ describe('Config', function() {
         config.options.ignoreMultipleAssignIdentifier = true;
         config.options.trimLines = false;
         var result = config.decode(data);
-        expect(result.a).to.equal("1");
-        expect(result.b).to.equal("2");
-        expect(result.c).to.equal("3\t");
+        expect(result.a).toBe("1");
+        expect(result.b).toBe("2");
+        expect(result.c).toBe("3\t");
     })
 });
--- test.js
+++
@@ -1,5 +0,0 @@
-var Mocha = require('mocha');
-var mocha = new Mocha({});
-mocha.addFile('./conf-cfg-ini.spec');
-mocha.run();
-
